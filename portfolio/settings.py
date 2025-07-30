8fadd386bc02b644f7f9755b92306a954c542a41
Asiwaju24 <150304604+Asiwaju24@users.noreply.github.com>
2025-07-30 08:09:43 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,6 +45,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 
@@ -114,6 +116,10 @@ X_FRAME_OPTIONS = None
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+  
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+  
 
 STATIC_URL = '/static/'  # URL to access static files in templates
 
